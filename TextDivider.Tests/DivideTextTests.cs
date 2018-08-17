75580a7963db7f2c1248e3e96108ec45187fb292
Mantas Zambacevicius <mantas.zambacevicius@icybit.co>
2018-08-17 11:07:15 +0300

    initial commit

---
+++ TextDivider.Tests/DivideTextTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextDivider.Tests
+{
+    [TestClass]
+    public class DivideTextTests
+    {
+        [TestMethod]
+        public void Divides_Text_To_Lines_Correctly()
+        {
+			var textDivider = new TextDivider();
+			var result = textDivider.DivideText("þodis þodis þodis", 13);
+
+			Assert.AreEqual(2, Regex.Matches(result, "\n").Count);
+
+			var result2 = textDivider.DivideText("ðiuolaikiðkas ir mano þodis", 7);
+
+			Assert.AreEqual(4, Regex.Matches(result2, "\n").Count);
+		}
+
+		[TestMethod]
+		public void Divided_Text_In_Line_Is_Correct()
+		{
+			var textDivider = new TextDivider();
+			var result = textDivider.DivideText("þodis þodis þodis", 13);
+			ReadStringLines(result, (line) => Assert.IsTrue(line.Count(c => !Char.IsWhiteSpace(c)) <= 13));
+
+			var result2 = textDivider.DivideText("ðiuolaikiðkas ir mano þodis", 7);
+			ReadStringLines(result2, (line) => Assert.IsTrue(line.Count(c => !Char.IsWhiteSpace(c)) <= 7));
+		}
+
+		private void ReadStringLines(string text, Action<string> func = null)
+		{
+			using (var reader = new StringReader(text))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					func(line);
+				}
+			}
+		}
+    }
+}
---
+++ TextDivider/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TextDivider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var textDivider = new TextDivider();
+			var lines = File.ReadLines(@"Data\Input.txt");
+			var results = new List<string>();
+
+			foreach (var line in lines)
+			{
+				var numberEnd = line.IndexOf(' ');
+				var lineLetterCount = Convert.ToInt32(line.Substring(0, numberEnd));
+				var text = line.Substring(numberEnd);
+				results.Add(textDivider.DivideText(text, lineLetterCount));
+			}
+
+			File.WriteAllLines(@"Data\Results.txt", results);
+        }
+	}
+}
